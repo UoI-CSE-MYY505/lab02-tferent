44038525bbd16d60ce50095e4819a69b07ff6ba3
Theodora Ferentinou <cs05379@uoi.gr>
2024-10-22 21:24:25 +0300

    Done

--- lab02.s
+++ lab02.s
@@ -9,10 +9,20 @@ array: .word 1, 0, 1, 12, 0, 1, 4
     li a1, 7    # unsigned
     li a2, 1
 prog:
-#-----------------------------
-# Write your code here!
-# Do not remove the prog label or write code above it!
-#-----------------------------
+    add  s0, zero, zero 
+loop1:
+    beq  a1, zero, done1
+    lw   t1, 0(a0)
+    bne  t1, a2, next1
+    add  s0, a0, zero 
+next1:  
+    addi a0, a0, 4
+    addi a1, a1, -1
+    j    loop1
+done1:
+    addi a7, zero, 10 
+    ecall
+
 done:
     addi a7, zero, 10 
     ecall
